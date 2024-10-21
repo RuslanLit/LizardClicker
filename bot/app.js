a95d0cc66d4843bf61e958016ba908be6d635432
RuslanLit <52565744+RuslanLit@users.noreply.github.com>
2024-10-21 19:43:07 +0300

    first commit

---
+++ bot/app.js
@@ -0,0 +1,15 @@
+import {telegraf, Markup, Telegraf} from 'telegraf'
+
+const token = '7871819035:AAExNeq7wScjTn-Xhk1lToDrKJDMHmqvyPc'
+const webAppUrl = 'https://test-lizard-test.web.app/'
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+    ctx.reply(
+        'Привіт! Починаємо гру! )',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('Open mini app', `${webAppUrl}?ref=${ctx.payload}`)
+        ])
+    )
+})
